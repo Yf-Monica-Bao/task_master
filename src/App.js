99c954e7d33e88e84d4d74e8f7b09dda2b63c782
Yufei Bao <ybao1005@gmail.com>
2023-02-21 14:21:31 -0800

    readme added

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import {
 import { async } from "@firebase/util";
 import AddTaskForm from "./addTaskForm";
 import TasksTable from "./components/tasksTable";
+import UpdateTaskForm from "./updateTask";
 import { Table } from "react-bootstrap";
 
 import Form from "react-bootstrap/Form";
